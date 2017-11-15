17ea1a2314e5d1d0b21821f9417de95db7d1ddce
alkor97 <alkor@konto.pl>
2017-11-15 19:51:02 +0100

    Added support for starting StreetView application when run from Chrome on Android.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,30 @@ import { isStreetViewSupportedAt } from './has-street-view';
 import * as LT from './leaflet-textpath';
 import { ParsedQuery, parseQuery, Query } from './query-parser';
 
+function isChromeOnAndroid() {
+  const ua = navigator.userAgent;
+  return ua.match(/android/i) && ua.match(/chrome/i);
+}
+
+function getStreetViewLink(q: Query): string {
+  const prefix = 'http://maps.google.com/maps?q=&layer=c&cbll=';
+  const regularLink = `${prefix}${q.lat},${q.lng}&cbp=0,${q.bearing},0,0,0`;
+  if (isChromeOnAndroid()) {
+    const fallback = encodeURI(regularLink);
+    return 'intent://view/#Intent;package=com.google.android.apps.maps;scheme=google.streetview:'
+      + `cbll=${q.lat},${q.lng}&cbp=0,${q.bearing},0,0,0;`
+      + `S.browser_fallback_url=${fallback};end;`;
+  }
+  return regularLink;
+}
+
 function preparePopup(q: Query): string {
   const withStreetView = q.hasStreetView ? 'inline' : 'none';
-  const prefix = 'http://maps.google.com/maps?q=&layer=c&cbll=';
+  const link = getStreetViewLink(q);
   return `<b>${q.name}</b><br>
           <sup><i>${q.phone}</i></sup>
           <div><a style='display: ${withStreetView}'
-            href='${prefix}${q.lat},${q.lng}&cbp=11,${q.bearing},0,0,0' target='_blank'>
+            href='${link}' target='_blank'>
             <img src="img/eye.png" width="16" height="16"/>
           </a></div>`;
 }
