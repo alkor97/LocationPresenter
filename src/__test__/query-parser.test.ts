f33c602eaa6944c3afe0ae66c1c2795991d45fc6
alkor97 <alkor@konto.pl>
2017-11-20 22:06:55 +0100

    Added support for parsing provider and timestamp.

--- src/__test__/query-parser.test.ts
+++ src/__test__/query-parser.test.ts
@@ -3,6 +3,8 @@ import { ParsedQuery, parseQuery } from '../query-parser';
 
 describe('QueryParser', () => {
   const expected = {
+    date: new Date(2015, 3, 2, 15, 14, 13, 213),
+    provider: 'network',
     lat: 12.3,
     lng: 23.4,
     alt: 34,
@@ -19,30 +21,36 @@ describe('QueryParser', () => {
   it('parses values correctly', () => {
     const name = encodeURIComponent(expected.name);
     const phone = encodeURIComponent(expected.phone);
+    const date = expected.date.toISOString();
     const actual = parseQuery(`?lat=${expected.lat}&lng=${expected.lng}&alt=${expected.alt}&radius=${expected.radius}`
       + `&name=${name}&phone=${phone}&bearing=${expected.bearing}&speed=${expected.speed}`
-      + `&dbg=${expected.dbg}`);
+      + `&dbg=${expected.dbg}&date=${date}&provider=${expected.provider}`);
     expect(actual).toEqual(expected);
   });
 
   it('parses CSV query correctly', () => {
     const name = encodeURIComponent(expected.name);
     const phone = encodeURIComponent(expected.phone);
-    const actual = parseQuery(`?q=${expected.lat},${expected.lng},${expected.alt},${expected.radius},`
-      + `${name},${phone},${expected.bearing},${expected.speed},${expected.dbg}`);
+    const date = expected.date.toISOString();
+    const actual = parseQuery(`?q=${date},${expected.provider},${expected.lat},${expected.lng},`
+      + `${expected.alt},${expected.radius},${name},${phone},${expected.bearing},${expected.speed},`
+      + `${expected.dbg}`);
     expect(actual).toEqual(expected);
   });
 
   it('returns defaults on error', () => {
-    expect(parseQuery('')).toEqual(new ParsedQuery());
-    expect(parseQuery('?')).toEqual(new ParsedQuery());
-    expect(parseQuery('?q')).toEqual(new ParsedQuery());
-    expect(parseQuery('?q=')).toEqual(new ParsedQuery());
+    const q = new ParsedQuery();
+    q.date = new Date();
+    q.date.setMilliseconds(0);
+    expect(parseQuery('')).toEqual(q);
+    expect(parseQuery('?')).toEqual(q);
+    expect(parseQuery('?q')).toEqual(q);
+    expect(parseQuery('?q=')).toEqual(q);
   });
 
   it('returns defaults on missing CSV component', () => {
     const phone = encodeURIComponent(expected.phone);
-    const actual = parseQuery(`?q=${expected.lat},${expected.lng},${expected.alt},${expected.radius},`
+    const actual = parseQuery(`?q=,,${expected.lat},${expected.lng},${expected.alt},${expected.radius},`
       + `,${phone},,${expected.speed}`);
     const defaults = new ParsedQuery();
     expect(actual.name).toEqual(defaults.name);
--- src/query-parser.ts
+++ src/query-parser.ts
@@ -1,5 +1,11 @@
 
+export enum Provider {
+  UNKNOWN = '?', GPS = 'gps', NETWORK = 'network',
+}
+
 export interface Query {
+  readonly date: Date;
+  readonly provider?: Provider;
   readonly lat: number;
   readonly lng: number;
   readonly alt: number;
@@ -12,13 +18,21 @@ export interface Query {
   readonly dbg?: number;
 }
 
+function now(): Date {
+  const date = new Date();
+  date.setMilliseconds(0);
+  return date;
+}
+
 export class ParsedQuery {
+  public date: Date = now();
+  public provider: Provider = Provider.UNKNOWN;
   public lat: number = 53.4296143;
   public lng: number = 14.5445406;
   public alt: number = 37;
   public radius: number = 750;
-  public name: string = 'Krysia';
-  public phone: string = '+48123456789';
+  public name: string = 'Frania';
+  public phone: string = '+48 123 456 789';
   public bearing?: number;
   public speed?: number;
   public hasStreetView: boolean = false;
@@ -27,6 +41,7 @@ export class ParsedQuery {
 }
 
 const NUMBER_PROPERTIES = {lat: 1, lng: 1, alt: 1, radius: 1, bearing: 1, speed: 1, dbg: 1};
+const DATE_PROPERTIES = {date: 1};
 
 export function parseQuery(queryString: string): Query {
   const q = queryString.trim().replace(/^\?/, '');
@@ -37,8 +52,26 @@ export function parseQuery(queryString: string): Query {
   }
 }
 
+function parseValue(output: ParsedQuery, key: string, value: string): ParsedQuery {
+  if (key in NUMBER_PROPERTIES) {
+    output[key] = parseFloat(value);
+  } else if (key in DATE_PROPERTIES) {
+    output[key] = new Date(Date.parse(value));
+  } else if (key === 'provider') {
+    if (value === 'gps') {
+      output[key] = Provider.GPS;
+    } else if (value === 'network') {
+      output[key] = Provider.NETWORK;
+    }
+  } else {
+    output[key] = value;
+  }
+  return output;
+}
+
 function parseCSVQuery(csv: string): Query {
-  const positionMapping = ['lat', 'lng', 'alt', 'radius', 'name', 'phone', 'bearing', 'speed', 'dbg'];
+  const positionMapping = ['date', 'provider', 'lat', 'lng', 'alt', 'radius', 'name', 'phone',
+    'bearing', 'speed', 'dbg'];
   return csv.replace(/^q[\=]/, '')
     .split(',')
     .reduce((state: ParsedQuery, entry: string, position: number) => {
@@ -46,11 +79,7 @@ function parseCSVQuery(csv: string): Query {
       if (key) {
         const value = decodeURIComponent(entry);
         if (value) {
-          if (key in NUMBER_PROPERTIES) {
-            state[key] = parseFloat(value);
-          } else {
-            state[key] = value;
-          }
+          parseValue(state, key, value);
         }
       }
       return state;
@@ -66,11 +95,7 @@ function parseObjectQuery(queryString: string): Query {
       const key = decodeURIComponent(pair[0]);
       if (key in allowed) {
         const value = decodeURIComponent(pair[1]);
-        if (key in NUMBER_PROPERTIES) {
-          state[key] = parseFloat(value);
-        } else {
-          state[key] = value;
-        }
+        state = parseValue(state, key, value);
       }
       return state;
     }, new ParsedQuery());
