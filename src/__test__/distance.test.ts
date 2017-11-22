7f0666b7534fa7addcd85362ae85f9faa2e92231
alkor97 <alkor@konto.pl>
2017-11-22 22:16:33 +0100

    Distance concept reimplemented.

---
+++ src/__test__/distance.test.ts
@@ -0,0 +1,48 @@
+
+import * as D from '../distance';
+
+describe('Distance', () => {
+    it('is created correctly', () => {
+        let value = 0;
+        // tslint:disable-next-line:forin
+        for (const unit in D.Unit) {
+            const d = new D.Distance(++value, unit as D.Unit);
+            expect(d.value).toEqual(value);
+            expect(d.unit).toEqual(unit);
+        }
+    });
+    it('is correctly multiplied', () => {
+        let value = 0;
+        // tslint:disable-next-line:forin
+        for (const unit in D.Unit) {
+            const d = new D.Distance(++value, unit as D.Unit).multiply(2);
+            expect(d.value).toEqual(2 * value);
+            expect(d.unit).toEqual(unit);
+        }
+    });
+    it('returns itself on multiplication by 1', () => {
+        const d = D.meters(13);
+        expect(d.multiply(1)).toBe(d);
+    });
+    it('is correctly converted', () => {
+        expect(D.kilometers(1).to(D.Unit.METERS).value).toEqual(1000);
+    });
+    it('returns itself on no-change conversion', () => {
+        const d = D.meters(13);
+        expect(d.to(D.Unit.METERS)).toBe(d);
+    });
+    it('constructor returns defaults when called without parameters', () => {
+        const d = new D.Distance();
+        expect(d.value).toEqual(0);
+        expect(d.unit).toEqual(D.Unit.METERS);
+    });
+    it('dedicated methods return correct distances', () => {
+        expect(D.pixels(1)).toEqual(new D.Distance(1, D.Unit.PIXELS));
+        expect(D.meters(1)).toEqual(new D.Distance(1, D.Unit.METERS));
+        expect(D.kilometers(1)).toEqual(new D.Distance(1, D.Unit.KILOMETERS));
+        expect(D.feet(1)).toEqual(new D.Distance(1, D.Unit.FEET));
+        expect(D.yards(1)).toEqual(new D.Distance(1, D.Unit.YARDS));
+        expect(D.miles(1)).toEqual(new D.Distance(1, D.Unit.MILES));
+        expect(D.nauticalMiles(1)).toEqual(new D.Distance(1, D.Unit.NAUTICAL_MILES));
+    });
+});
---
+++ src/distance.ts
@@ -0,0 +1,77 @@
+
+export enum Unit {
+    PIXELS = 'px',
+    METERS = 'm',
+    KILOMETERS = 'km',
+    FEET = 'ft',
+    YARDS = 'yd',
+    MILES = 'mi',
+    NAUTICAL_MILES = 'nm',
+}
+
+class OneMeter {
+    public readonly m = 1;
+    public readonly km = 1000;
+    public readonly ft = 0.3048;
+    public readonly yd = 0.9144;
+    public readonly mi = 1609.344;
+    public readonly nm = 1852;
+    readonly [key: string]: number;
+}
+
+const ONE_METER = new OneMeter();
+
+// tslint:disable-next-line:max-classes-per-file
+export class Distance {
+    public readonly value: number;
+    public readonly unit: Unit;
+
+    constructor(value: number = 0, unit: Unit = Unit.METERS) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public to(unit: Unit): Distance | this {
+        if (unit !== this.unit) {
+            const source = this.value * ONE_METER[this.unit];
+            const target = source / ONE_METER[unit];
+            return new Distance(target, unit);
+        }
+        return this;
+    }
+
+    public multiply(value: number): Distance | this {
+        if (value !== 1) {
+            return new Distance(this.value * value, this.unit);
+        }
+        return this;
+    }
+}
+
+export function pixels(value: number): Distance {
+    return new Distance(value, Unit.PIXELS);
+}
+
+export function meters(value: number): Distance {
+    return new Distance(value, Unit.METERS);
+}
+
+export function kilometers(value: number): Distance {
+    return new Distance(value, Unit.KILOMETERS);
+}
+
+export function feet(value: number): Distance {
+    return new Distance(value, Unit.FEET);
+}
+
+export function yards(value: number): Distance {
+    return new Distance(value, Unit.YARDS);
+}
+
+export function miles(value: number): Distance {
+    return new Distance(value, Unit.MILES);
+}
+
+export function nauticalMiles(value: number): Distance {
+    return new Distance(value, Unit.NAUTICAL_MILES);
+}
