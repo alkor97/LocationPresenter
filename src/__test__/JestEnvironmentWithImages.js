c6b14f52e3563a251bbf64d1abf0a92a984c4aba
alkor97 <alkor@konto.pl>
2017-12-02 23:32:02 +0100

    Corrections according to linter rules.

--- src/__test__/JestEnvironmentWithImages.js
+++ src/__test__/JestEnvironmentWithImages.js
@@ -1,21 +1,21 @@
 
 'use strict';
 
-var _jestUtil;
+var jestUtil;
 function _load_jestUtil() {
-  return _jestUtil = require('jest-util');
+  return jestUtil = require('jest-util');
 }
 
-var _jestMock;
+var jestMock;
 
 function _load_jestMock() {
-  return _jestMock = _interopRequireDefault(require('jest-mock'));
+  return jestMock = _interopRequireDefault(require('jest-mock'));
 }
 
-var _jsdom;
+var jsDom;
 
 function _load_jsdom() {
-  return _jsdom = _interopRequireDefault(require('jsdom'));
+  return jsDom = _interopRequireDefault(require('jsdom'));
 }
 
 function _interopRequireDefault(obj) {
@@ -25,7 +25,7 @@ function _interopRequireDefault(obj) {
 class JSDomEnvironmentWithImages {
   constructor(config) {
     // lazy require
-    this.document = (_jsdom || _load_jsdom()).default.jsdom('<!DOCTYPE html>', {
+    this.document = (jsDom || _load_jsdom()).default.jsdom('<!DOCTYPE html>', {
       url: config.testURL,
       // changes begin: added support for loading images
       features: {
@@ -38,10 +38,12 @@ class JSDomEnvironmentWithImages {
     // Node's error-message stack size is limited at 10, but it's pretty useful
     // to see more than that when a test fails.
     this.global.Error.stackTraceLimit = 100;
-    (0, (_jestUtil || _load_jestUtil()).installCommonGlobals)(global, config.globals);
 
-    this.moduleMocker = new (_jestMock || _load_jestMock()).default.ModuleMocker(global);
-    this.fakeTimers = new (_jestUtil || _load_jestUtil()).FakeTimers(global, this.moduleMocker, config);
+    // tslint:disable-next-line:no-unused-expression
+    (0, (jestUtil || _load_jestUtil()).installCommonGlobals)(global, config.globals);
+
+    this.moduleMocker = new (jestMock || _load_jestMock()).default.ModuleMocker(global);
+    this.fakeTimers = new (jestUtil || _load_jestUtil()).FakeTimers(global, this.moduleMocker, config);
   }
 
   dispose() {
@@ -58,10 +60,10 @@ class JSDomEnvironmentWithImages {
 
   runScript(script) {
     if (this.global) {
-      return (_jsdom || _load_jsdom()).default.evalVMScript(this.global, script);
+      return (jsDom || _load_jsdom()).default.evalVMScript(this.global, script);
     }
     return null;
   }
-};
+}
 
 module.exports = JSDomEnvironmentWithImages;
