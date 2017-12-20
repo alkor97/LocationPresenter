5b75c8d49ff5a06d764e80ac8c16d5f99e1c6765
alkor@konto.pl <alkor@konto.pl>
2017-12-20 10:55:38 +0100

    Cache busting applied.

--- webpack.common.js
+++ webpack.common.js
@@ -48,7 +48,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/assets/index.html',
-      title: 'Location Presenter'
+      title: 'Location Presenter',
+      hash: true
     }),
     new ForkTsCheckerWebpackPlugin({
       tslint: true
